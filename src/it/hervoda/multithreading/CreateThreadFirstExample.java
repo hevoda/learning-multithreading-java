9c02de6cedc5bd651c46faf9b2e455163196b1cc
hevoda <herve.evoda@outlook.com>
2026-05-01 19:52:56 +0200

    created threads with runnable tasks

--- src/it/hervoda/multithreading/CreateThreadFirstExample.java
+++ src/it/hervoda/multithreading/CreateThreadFirstExample.java
@@ -3,32 +3,22 @@ package it.hervoda.multithreading;
 
 public class CreateThreadFirstExample {
 
+    public static Object sharedObject = new Object();
+
     public static void main(String[] args) {
 
         System.out.println("Main Thread started!");
 
         Runnable objRunnable1 = () -> {
             System.out.println("Thread 1 started");
-            System.out.println(Thread.currentThread().getName());
-            System.out.println(Thread.currentThread().getState());
-            try {
-                Thread.sleep(2000);
-            }catch (InterruptedException e){
-                throw  new RuntimeException(e);
-            }
+            runningTask();
             System.out.println("Thread 1 completed");
 
         };
 
         Runnable objRunnable2 = () -> {
             System.out.println("Thread 2 started");
-            System.out.println(Thread.currentThread().getName());
-            System.out.println(Thread.currentThread().getState());
-            try {
-                Thread.sleep(2000);
-            }catch (InterruptedException e){
-                throw  new RuntimeException(e);
-            }
+            runningTask();
             System.out.println("Thread 2 completed");
 
         };
@@ -37,9 +27,21 @@ public class CreateThreadFirstExample {
         objThread1.start();
 
         Thread objThread2 = new Thread(objRunnable2 , "Runnable Thread 2");
+        //objThread2.setDaemon(true);
         objThread2.start();
 
         System.out.println("Main Thread ended!");
 
+    }
+
+    private static void runningTask() {
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getState());
+        System.out.println(sharedObject.hashCode());
+        try {
+            Thread.sleep(2000);
+        }catch (InterruptedException e){
+            throw  new RuntimeException(e);
+        }
     }
 }
